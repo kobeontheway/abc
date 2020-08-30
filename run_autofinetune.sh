e4c80f5dfe46a27efeded58cd90396b3a552cd9e
tongxin.yuan <tongxin.yuan@bianlifeng.com>
2020-08-30 15:36:48 +0800

    1

--- run_autofinetune.sh
+++ run_autofinetune.sh
@@ -4,7 +4,7 @@ hub autofinetune autofinetune.py \
     --param_file=hparam.yaml \
     --gpu=0 \
     --popsize=15 \
-    --round=30 \
+    --round=3 \
     --output_dir=${OUTPUT} \
     --evaluator=fulltrail \
     --tuning_strategy=pshe2
