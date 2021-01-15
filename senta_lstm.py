b30672f45a6fcc94f3e53e13375302958b3fcb08
tongxin.yuan <tongxin.yuan@bianlifeng.com>
2021-01-15 19:24:11 +0800

    1

--- senta_lstm.py
+++ senta_lstm.py
@@ -1,16 +1,20 @@
-# coding: utf8
 import requests
 import json
-# 中文情感分析-done
 
-if __name__ == "__main__":
-    # 指定用于预测的文本并生成字典{"text": [text_1, text_2, ... ]}
-    text_list = ["我不爱吃甜食", "我喜欢躺在床上看电影"]
-    data = {"text": text_list}
-    # 指定预测方法为senta_lstm并发送post请求
-    url = "http://10.1.12.33:8866/predict/text/senta_lstm"
+# 中文情感分析
 
-    r = requests.post(url=url,  data=data)
+# 待预测数据
+text = ["这家餐厅很好吃", "这部电影真的很差劲"]
 
-    # 打印预测结果
-    print(json.dumps(r.json(), indent=4, ensure_ascii=False))
+# 设置运行配置
+# 对应本地预测senta_lstm.sentiment_classify(texts=text, batch_size=1, use_gpu=True)
+data = {"texts": text, "batch_size": 1, "use_gpu":True}
+
+# 指定预测方法为senta_lstm并发送post请求，content-type类型应指定json方式
+# HOST_IP为服务器IP
+url = "http://10.1.12.33:8866/predict/senta_lstm"
+headers = {"Content-Type": "application/json"}
+r = requests.post(url=url, headers=headers, data=json.dumps(data))
+
+# 打印预测结果
+print(json.dumps(r.json(), indent=4, ensure_ascii=False))
