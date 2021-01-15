ac14c39b2c16743646e70042cf93585bbaf9a357
tongxin.yuan <tongxin.yuan@bianlifeng.com>
2021-01-15 15:07:03 +0800

    1

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,4 @@
 hub serving start --config server_config.json
+hub serving start bert_service -m ernie_tiny --use_gpu --gpu 0 --port 8866
+hub serving start -m senta_lstm --port 8866
+hub serving start -m vgg11_imagenet --port 8866
