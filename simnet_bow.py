cd6d3c7b96cb1ddd8333513dd3d4597e8b887d3e
tongxin.yuan <tongxin.yuan@bianlifeng.com>
2021-01-15 17:54:10 +0800

    1

--- simnet_bow.py
+++ simnet_bow.py
@@ -1,19 +1,24 @@
-# coding: utf8
 import requests
 import json
-# 中文语义分析
 
-if __name__ == "__main__":
-    # 指定用于匹配的文本并生成字典{"text_1": [text_a1, text_a2, ... ]
-    #                              "text_2": [text_b1, text_b2, ... ]}
+# 待预测数据
+test_text_1 = ["这道题太难了", "这道题太难了", "这道题太难了"]
+test_text_2 = ["这道题是上一年的考题", "这道题不简单", "这道题很有意思"]
 
-    text = {
-        "text_1": ["这道题太难了", "这道题太难了", "这道题太难了"],
-        "text_2": ["这道题是上一年的考题", "这道题不简单", "这道题很有意思"]
-    }
-    # 指定匹配方法为simnet_bow并发送post请求
-    url = "http://10.1.12.33:8866/predict/text/simnet_bow"
-    r = requests.post(url=url, data=text)
+text = [test_text_1, test_text_2]
 
-    # 打印匹配结果
-    print(json.dumps(r.json(), indent=4, ensure_ascii=False))
+# 设置运行配置
+# 对应本地预测simnet_bow.similarity(texts=text, batch_size=1, use_gpu=True)
+data = {"texts": text, "batch_size": 1, "use_gpu":True}
+
+# 指定预测方法为simnet_bow并发送post请求，content-type类型应指定json方式
+# HOST_IP为服务器IP
+url = "http://10.1.12.33:8866/predict/simnet_bow"
+headers = {"Content-Type": "application/json"}
+r = requests.post(url=url, headers=headers, data=json.dumps(data))
+
+# 打印预测结果
+print(json.dumps(r.json(), indent=4, ensure_ascii=False))
+
+# 命令行模式：
+# hub run simnet_bow --text_1 "这道题太难了" --text_2 "这道题不简单"
